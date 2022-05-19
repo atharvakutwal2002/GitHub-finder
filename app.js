e010af0a9d958016006566350346d2b3dedc8e6b
Atharva <atharvakutwal2002@gmail.com>
2022-05-19 19:55:33 +0530

    final commit

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-document.getElementById("searchUser").addEventListener("onChange", onChangeHandler);
+// Init Github
+const github = new Github;
+// Init UI
+const ui = new UI;
 
+// Search input
+const searchUser = document.getElementById('searchUser');
 
-function onChangeHandler(e){
-    
-}
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+}); 
